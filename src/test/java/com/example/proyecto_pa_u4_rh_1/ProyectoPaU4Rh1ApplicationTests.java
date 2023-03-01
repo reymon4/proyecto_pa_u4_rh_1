d2e233b76b970cd00b2bf747724bf1feba73854c
Reymon <rahidalgob@uce.edu.ec>
2023-02-28 19:43:51 -0500

    Vista-Controller (MVC)

---
+++ src/test/java/com/example/proyecto_pa_u4_rh_1/ProyectoPaU4Rh1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_pa_u4_rh_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPaU4Rh1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
